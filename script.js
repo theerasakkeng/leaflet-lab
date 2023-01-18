d8433f563a40e971901ab6aa4ffe5c5c17222144
theerasakph <theerasak.ph@chococrm.com>
2023-01-18 15:27:40 +0700

    [Add] minimap

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//base map
 var streetMap = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
@@ -6,26 +7,59 @@ var streetMap = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 var Esri_WorldImagery = L.tileLayer(
   "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
   {
+    maxZoom: 19,
     attribution:
       "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
   }
 );
+const miniTileMap = new L.TileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  {
+    maxZoom: 19,
+    attribution:
+      "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+  }
+);
+
+//get center and set
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      mapOption.center = [];
+      mapOption.center.push(position.coords.latitude);
+      mapOption.center.push(position.coords.longitude);
+    });
+  }
+};
+
+// map opption
 const mapOption = {
   zoomControl: false,
   zoom: 10,
+  center: [13.6970314, 100.608406],
   doubleClickZoom: false,
   layers: [streetMap],
 };
+getLocation();
+
+//add map
 var map = L.map("map", mapOption);
+//add minimap
+var miniMap = new L.Control.MiniMap(miniTileMap, {}).addTo(map);
+
 var baseMaps = {
   OpenStreetMap: streetMap,
   WorldMap: Esri_WorldImagery,
 };
+
+//add basemap to control
 var layerControl = L.control
   .layers(baseMaps, null, {
     position: "topleft",
   })
   .addTo(map);
+
+//getlocation and setview
 const setLocation = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -38,26 +72,34 @@ const setLocation = () => {
   }
 };
 setLocation();
+
+//add zoom control
 L.control
   .zoom({
-    position: "topright",
+    position: "topright", //"topleft" || "bottomright" || "bottomleft"
   })
   .addTo(map);
+
+//add scale control
 L.control.scale().addTo(map);
-map.on("load", () => {
+
+//getdefault center and zoom to html
+const getDefault = () => {
+  let zoomlevel = map.getZoom();
   let center = map.getCenter();
+  document.querySelector(".zoom-level").innerHTML = zoomlevel;
   document.querySelector(".current-center").innerHTML = `${center.lat},
     ${center.lng}`;
-});
-const getzoomDefault = () => {
-  let zoomlevel = map.getZoom();
-  document.querySelector(".zoom-level").innerHTML = zoomlevel;
 };
-getzoomDefault();
+getDefault();
+
+//getzoom when zoomed event
 map.on("zoomend", () => {
   let zoomlevel = map.getZoom();
   document.querySelector(".zoom-level").innerHTML = zoomlevel;
 });
+
+//get latlong when mousemove event
 map.on("mousemove", (e) => {
   document.querySelector(".lat").innerHTML = e.latlng.lat;
   document.querySelector(".long").innerHTML = e.latlng.lng;
@@ -65,32 +107,82 @@ map.on("mousemove", (e) => {
   document.querySelector(".current-center").innerHTML = `${center.lat},
     ${center.lng}`;
 });
+
+//create marker when double click
 map.on("dblclick", (e) => {
-  let markerEl = new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map)
-    .bindPopup(`${e.latlng.lat},
+  let markerEl = new L.marker(e.latlng).addTo(map).bindPopup(`${e.latlng.lat},
         ${e.latlng.lng}`);
 });
 
-fetch("./TAMBON.json.geojson")
-  .then((res) => res.json())
-  .then((data) => {
-    var tambon = L.geoJSON(data.features, {
-      color: "#ff7800",
+//get geojson
+const getGeojson = async () => {
+  //Village geojson
+  await fetch("./geojson/VILLAGE.geojson")
+    .then((res) => res.json())
+    .then((data) => {
+      const village = L.geoJSON(data.features);
+      layerControl.addOverlay(village, "Village");
+      village.on("click", async (e) => {
+        await map.flyTo(e.latlng, 17);
+        L.popup({
+          offset: L.point(1, -25),
+        })
+          .setLatLng(e.latlng)
+          .setContent(e.layer.feature.properties.VILL_NM_T)
+          .openOn(map);
+      });
     });
-    layerControl.addOverlay(tambon, "Tambon");
-    tambon.on("click", async (e) => {
-      console.log(e)
-      let layerCenter = await e.layer.getCenter();
-      await tambon.resetStyle();
-      await e.layer.setStyle({ fillColor: "#0000FF" });
-      await map.flyTo([layerCenter.lat, layerCenter.lng], 12);
-      let center = await map.getCenter();
-      document.querySelector(".current-center").innerHTML = `${center.lat},
-    ${center.lng}`;
-      document.querySelector(
-        ".tambon-detail"
-      ).innerHTML = `<span>ตำบล ${e.layer.feature.properties.TAM_NAMT}</span>
-      <span>อำเภอ ${e.layer.feature.properties.AMP_NAMT}</span>
-      <span>จังหวัด ${e.layer.feature.properties.PROV_NAMT}</span><div>พื้นที่ ${e.layer.feature.properties.PERIMETER} ไร่</div>`;
+
+  //Tambon geojson
+  await fetch("./geojson/TAMBON.geojson")
+    .then((res) => res.json())
+    .then((data) => {
+      const tambon = L.geoJSON(data.features, {
+        color: "#ff7800",
+        weight: 1,
+      });
+      layerControl.addOverlay(tambon, "Tambon");
+      tambon.on("click", async (e) => {
+        let layerCenter = await e.layer.getCenter();
+        await tambon.resetStyle();
+        await e.layer.setStyle({ fillColor: "green" });
+        await map.flyTo([layerCenter.lat, layerCenter.lng], 12);
+        let center = await map.getCenter();
+        document.querySelector(".current-center").innerHTML = `${center.lat},
+  ${center.lng}`;
+        document.querySelector(".tambon-detail").innerHTML = `<span>ตำบล ${
+          e.layer.feature.properties.TAM_NAMT
+        }</span>
+    <span>อำเภอ ${e.layer.feature.properties.AMP_NAMT}</span>
+    <span>จังหวัด ${e.layer.feature.properties.PROV_NAMT}</span><div>พื้นที่ ${
+          e.layer.feature.properties.AREA / 1600
+        } ไร่</div>`;
+      });
     });
-  });
+
+  //Amphoe geojson
+  await fetch("./geojson/AMPHOE.geojson")
+    .then((res) => res.json())
+    .then((data) => {
+      const amphoe = L.geoJSON(data.features, {
+        color: "red",
+      });
+      layerControl.addOverlay(amphoe, "Amphoe");
+      amphoe.on("click", async (e) => {
+        let layerCenter = await e.layer.getCenter();
+        await amphoe.resetStyle();
+        await e.layer.setStyle({ fillColor: "#0000FF" });
+        await map.flyTo([layerCenter.lat, layerCenter.lng], 11);
+        let center = await map.getCenter();
+        document.querySelector(".current-center").innerHTML = `${center.lat},
+  ${center.lng}`;
+        document.querySelector(".amphoe-detail").innerHTML = `
+    <span>อำเภอ ${e.layer.feature.properties.AMP_NAMT}</span>
+    <span>จังหวัด ${e.layer.feature.properties.PROV_NAMT}</span><div>พื้นที่ ${
+          e.layer.feature.properties.AREA / 1600
+        } ไร่</div>`;
+      });
+    });
+};
+
+getGeojson();
